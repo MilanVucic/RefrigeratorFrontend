1c65970d0835f41d5c9b815880daa11cb07f0547
vucic <vucic94@yahoo.com>
2025-11-29 12:05:21 +0100

    Saving access token in local storage for easier testing

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -7,13 +7,14 @@ interface AuthState {
 
 export const useAuthStore = defineStore('auth', {
     state: (): AuthState => ({
-        accessToken: null,
+        accessToken: localStorage.getItem('accessToken'),
     }),
     actions: {
         async login(username: string, password: string) {
             try {
                 const response = await api.post<{ access: string }>('accounts/login/', { username, password });
                 this.accessToken = response.data.access;
+                localStorage.setItem('accessToken', this.accessToken);
             } catch (err: any) {
                 throw new Error(err.response?.data?.detail || 'Login failed');
             }
@@ -23,14 +24,17 @@ export const useAuthStore = defineStore('auth', {
             try {
                 const response = await api.post<{ access: string }>('accounts/token/refresh/');
                 this.accessToken = response.data.access;
+                localStorage.setItem('accessToken', this.accessToken);
             } catch (err: any) {
                 this.accessToken = null;
+                localStorage.removeItem('accessToken');
                 throw new Error('Refresh token expired');
             }
         },
 
         logout() {
             this.accessToken = null;
+            localStorage.removeItem('accessToken');
         },
     },
 });
